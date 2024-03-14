78e517f534e4d3a622d9b150d1af09528384df9b
Stranger <hassangamer247@gmail.com>
2024-03-14 21:13:52 +0500

    Twitter Clone

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black':'#16181c',
+        'custom-blue': '#1d9bff',
+      }
+    },
+  },
+  plugins: [],
+}
+
